f00c4ff0f24e87340ef0d66df004786ceb1aa455
Meraj Kashi <meraj.mostamand.kashi@gmail.com>
2022-11-27 20:39:39 +0000

    Docker installer

---
+++ docker-installation.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# A script to install Docker Engine
+
+# Remove old installation packages
+echo "Remove old installation packages"
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Install dependenvies packages
+echo "Install dependenvies packages"
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
+# Add Docker’s official GPG key
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Setup repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Add docker group to the current user
+sudo groupadd docker
+sudo usermod -aG docker $USER
