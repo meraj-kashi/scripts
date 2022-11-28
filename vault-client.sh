762ee86824f12a6f99f74473aec02604d17e8a05
Meraj Kashi <meraj.mostamand.kashi@gmail.com>
2022-11-28 22:03:02 +0000

    Added Vault client installation script

---
+++ vault-client.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Vault binary as client
+echo "Install Vault client"
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install -y vault
+
+if [[ $? == 0 ]]; then
+    echo "Vault instlled succesfully"
+fi
